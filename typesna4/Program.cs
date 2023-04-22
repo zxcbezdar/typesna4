0f23e36d8e85d5f91349faf223fcfedd9ca64796
zxcbezdar <maksikplayb@gmail.com>
2023-04-22 11:07:11 +0300

    commit

---
+++ typesna4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = "Объект – категория, выражающая то, что противостоит субъекту в его предметно-практической и познавательной деятельности.";
+        Console.WriteLine($"ASCII: {Encoding.ASCII.GetByteCount(text)} bytes");
+        Console.WriteLine($"UTF-7: {Encoding.UTF7.GetByteCount(text)} bytes");
+        Console.WriteLine($"UTF-8: {Encoding.UTF8.GetByteCount(text)} bytes");
+        Console.WriteLine($"UTF-16: {Encoding.Unicode.GetByteCount(text)} bytes");
+        Console.WriteLine($"UTF-32: {Encoding.UTF32.GetByteCount(text)} bytes");
+    }
+}
